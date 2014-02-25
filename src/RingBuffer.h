55137eee24b39cb45ce3c616c160bef5210778c5
Thibault Martin <thibault.a.martin@gmail.com>
2014-02-25 19:11:57 +0100

    Refactored RingBuffer & corrected behavior in error cases

--- src/RingBuffer.h
+++ src/RingBuffer.h
@@ -11,18 +11,28 @@ class RingBuffer {
 private:
     T *m_buffer;
     const size_t m_size;
-    size_t m_head;
-    size_t m_tail;
+    size_t m_back;
+    size_t m_front;
+    bool m_full;
     
     size_t next(size_t current) {
         return (current+1) % m_size;
     }
     
+    size_t prev(size_t current) {
+        return (current-1) % m_size;
+    }
+    
+    size_t pos_in_buffer(size_t pos) {
+        return (m_back+pos) % m_size;
+    }
+    
 public:
     RingBuffer(size_t size) :
     m_size(size),
-    m_head(0),
-    m_tail(0) {
+    m_back(0),
+    m_front(0),
+    m_full(false) {
         m_buffer = new T[size];
     }
     
@@ -30,25 +40,35 @@ public:
         delete [] m_buffer;
     }
     
-    void push(const T &object) {
+    void push_front(const T &object) {
         // Head is already placed at an "empty" space, storing object in buffer
-        m_buffer[m_head] = object;
+        m_buffer[m_front] = object;
         
         // Positionning head at new "empty" place
-        size_t next_head = next(m_head);
-        m_head = next_head;
+        m_front = next(m_front);
+        
+        // If just filled the buffer, notify it
+        if(m_front == m_back) {
+            m_full = true;
+        } else if(m_full) {
+            // If buffer was full, override previous values
+            m_back = next(m_back);
+        }
     }
     
-    void pop() {
-        // Nothing to do for deletion, just moving tail index
-        // Positioning tail at "new" first element inserted and not deleted
-        size_t next_tail = next(m_tail);
-        m_tail = next_tail;
+    void pop_front() {
+        if(m_full) {
+            m_full = false;
+            m_back = prev(m_back);
+        }
+        
+        m_front = prev(m_front);
     }
     
     const T& operator[](size_t pos) {
-        // No check on poss
-        return m_buffer[pos];
+        size_t real_pos = pos_in_buffer(pos);
+
+        return m_buffer[real_pos];
     }
     
     const T& at(size_t pos) {
@@ -56,8 +76,8 @@ public:
         if(pos >= m_size) {
             throw std::out_of_range("out of bounds");
         }
-        
-        return m_buffer[pos];
+        size_t real_pos = pos_in_buffer(pos);
+        return m_buffer[real_pos];
     }
 };
 
